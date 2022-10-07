8b4e1f222abec9401761d931dd328cd2622128e2
John <john.decorte01@gmail.com>
2022-10-07 11:33:45 +0200

    final?

--- containers/map.hpp
+++ containers/map.hpp
@@ -697,18 +697,6 @@ namespace ft
 
 	};
 
-	template<class InputIt1, class InputIt2>
-	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
-	{
-		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
-		{
-			if (*first1 < *first2)
-				return true;
-			if (*first2 < *first1)
-				return false;
-		}
-		return (first1 == last1) && (first2 != last2);
-	}
 
 	//
 	// Non-member functions (operators)
